b056a593f305342c1ff63c8242c06a49413d3c76
Deni <deni.setyawan@tiket.com>
2024-07-04 20:14:13 +0700

    add page

---
+++ src/app/accounts/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React, { useState } from 'react'
+
+interface InstagramAccount {
+  id: string;
+  username: string;
+  picture: string;
+}
+
+const page = () => {
+  const [accounts, setAccounts] = useState<InstagramAccount[] | null>(null)
+  const accessToken: string = 'EAAV71LZB2tg0BOZB87264sW0Qp6CMVLZC1LzWHnf4lyZAGtZAVQ1x0dxDuD4m1HktRAp9FBZAwtbKZBF15wk7FN9bwCNI4BLhVjH0lZCpwqc7ysZAQ5XIMqywygUt26wIMXo1vdVe8gtP6SnDZCOCj14SyAZCmWIGvsZBhxGbphGMEOezQQqEAZAEyMnWneSiQiP35FyOsR0TLvQZD'
+
+  const getInstagramAccounts = async () => {
+    const fetchInstagramAccounts = async () => {
+      try {
+        const response = await fetch(`https://graph.facebook.com/v20.0/me/accounts?access_token=${accessToken}&fields=id,username,profile_picture_url`);
+        const data = await response.json();
+        setAccounts(data.data);
+      } catch (error) {
+        console.error('Error listing Instagram Accounts:', error);
+      }
+    };
+
+    fetchInstagramAccounts();
+  }
+  
+  return (
+    <div>
+      {accessToken !== '' &&  <div>
+        <div>
+          <button onClick={getInstagramAccounts}>Get Instagram Accounts</button>
+        </div>
+        {accounts && accounts.map((account) => <div>{account.id}</div>)}
+      </div>}
+    </div>
+  )
+}
+
+export default page
--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,5 +1,6 @@
-'use client'
+"use client"
 import FacebookLogin, { SuccessResponse } from "@greatsumini/react-facebook-login";
+import { useState } from "react";
 
 interface FacebookResponse {
   name: string;
@@ -11,27 +12,63 @@ interface FacebookResponse {
   };
 }
 
-const oAuthPage = ({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | undefined};
-}) => {
+interface InstagramAccount {
+  id: string;
+  username: string;
+  picture: string;
+}
+
+const oAuthPage = () => {
+  const [accounts, setAccounts] = useState<InstagramAccount[] | null>(null)
+  const [accessToken, setAccesstoken] = useState('')
+  const [userId, setUserId] = useState('')
 
   const responseFacebook = (response: SuccessResponse) : SuccessResponse => {
     console.log(response);
+    setAccesstoken(response.accessToken)
+    setUserId(response.userID)
+
     // Handle the response here
     return response
   };
 
+  const getInstagramAccounts = async () => {
+    const fetchInstagramAccounts = async () => {
+      try {
+        const response = await fetch(`https://graph.facebook.com/v20.0/me/accounts?access_token=${accessToken}&fields=id,username,profile_picture_url`);
+        const data = await response.json();
+        setAccounts(data.data);
+      } catch (error) {
+        console.error('Error listing Instagram Accounts:', error);
+      }
+    };
+
+    fetchInstagramAccounts();
+  }
+
   return (
     <div>
-      <FacebookLogin
-        appId={'492789156539348'}
+      <h1>OAUTH</h1>
+     
+     <div>
+      <a href={`https://www.facebook.com/v20.0/dialog/oauth?client_id=1543528563258893&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=http://localhost:3001/success/&response_type=token&scope=instagram_basic,instagram_content_publish,instagram_manage_comments,instagram_manage_insights,pages_show_list,pages_read_engagement`}>
+        Login FB
+      </a>
+     </div>
+     {/* {accessToken === '' && <FacebookLogin
+        appId={'1543528563258893'}
         autoLoad={true}
         fields="name,email,picture"
-        scope="business_management"
+        scope="business_management,instagram_basic,pages_show_list,instagram_content_publish,instagram_manage_comments,instagram_manage_insights,instagram_manage_messages"
         onSuccess={responseFacebook}
-      />
+      /> }
+      {accessToken !== '' &&  <div>
+        <div>
+          <button onClick={getInstagramAccounts}>Get Instagram Accounts</button>
+        </div>
+        {accounts && accounts.map((account) => <div>{account.username}</div>)}
+      </div>} */}
+
     </div>
   )
 }
